b2ad6e178e644af6b5498776affe33f28beb5fbf
Alex Bramstång <89812175+Alexbrams@users.noreply.github.com>
2025-05-17 22:46:16 +0200

    Added ValueError handling
    Changed the inputs into functions to enable ValueError management

--- main.py
+++ main.py
@@ -2,39 +2,66 @@ import random
 import os
 
 # Necessary lists
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+
 def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
+
 
 # Clears the terminal
 clear_terminal()
 
 print("Welcome to the PyPassword Generator!")
 
+
 def main():
-
     # Inputs
-    nr_letters = int(input("\nHow many letters would you like in your password?\n"))
-    nr_symbols = int(input(f"How many symbols would you like?\n"))
-    nr_numbers = int(input(f"How many numbers would you like?\n"))
+    def letters_amount():
+        try:
+            l_amount = int(input("\nHow many letters would you like in your password?\n"))
+        except ValueError:
+            print("Invalid value, try again!")
+            return letters_amount()
+        return l_amount
 
+    def symbols_amount():
+        try:
+            s_amount = int(input(f"How many symbols would you like?\n"))
+        except ValueError:
+            print("Invalid value, try again!")
+            return symbols_amount()
+        return s_amount
+
+    def numbers_amount():
+        try:
+            n_amount = int(input(f"How many symbols would you like?\n"))
+        except ValueError:
+            print("Invalid value, try again!")
+            return numbers_amount()
+        return n_amount
+
+    nr_letters = letters_amount()
+    nr_symbols = symbols_amount()
+    nr_numbers = numbers_amount()
 
     # Empty character list
     random_characters = []
 
     # Appends the character list with letters
-    for number in range(0,nr_letters):
+    for number in range(0, nr_letters):
         random_characters.append(random.choice(letters))
 
     # Appends the character list with symbols
-    for number in range(0,nr_symbols):
+    for number in range(0, nr_symbols):
         random_characters.append(random.choice(symbols))
 
     # Appends the character list with numbers
-    for number in range(0,nr_numbers):
+    for number in range(0, nr_numbers):
         random_characters.append(random.choice(numbers))
 
     # Shuffles the character list
@@ -53,15 +80,15 @@ def main():
     else:
         clear_terminal()
         print(f'''
-            
+
             Your new password contains:
-            
+
               -{nr_letters} letters.
               -{nr_symbols} symbols. 
               -{nr_numbers} numbers.
-            
+
             Password: {password}
-                  
+
             ''')
         retry = input("\nDo you want to generate a new password? y/n?\n").lower()
         if retry == "y":
@@ -71,5 +98,7 @@ def main():
             clear_terminal()
             print("\nThanks for using my Password Generator!")
             return
+
+
 if __name__ == "__main__":
     main()
